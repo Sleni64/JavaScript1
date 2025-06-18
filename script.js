8c250bb8f1049b523ffb2e7d1ff38404f80d58d8
Sleni64 <Sarabellahleni@gmail.com>
2025-06-18 17:25:38 +0000

    add

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-document.getElementById("greeting").textContent ="Hi There!";
-document.getElementById("h1").textContent ="The Header";
 
-let items = document.getElementsByClassName("redclass");
-for (let i = 0; i < items.length; i++)
-{
-    items[i].textContent = "Text change";
-}
+const txtBox = document.getElementById("name");
 
-const header = document.querySelector("h1");
-console.log(header);
-const red = document.querySelectorAll(".redclass");
-console.log(red);
-document.getElementById("click").addEventListener("click", function()
+
+let name = "";
+const header = document.getElementById("h");
+
+document.getElementById("click").addEventListener("click",() =>
 {
-    alert("CLICKED BUTTON!");
+   event.preventDefault();
+   name = txtBox.value; 
+   header.textContent = "Welcome, " + name + "!";
 });
+
